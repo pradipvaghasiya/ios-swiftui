9ed93136a003bc64a3bb7db802399526cbd73d7a
Pradip Vaghasiya <pradip.vaghasiya@gmail.com>
2015-06-19 14:54:39 +0530

    Initial Commit

---
+++ SwiftUI/SwiftUI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUI.h
+//  SwiftUI
+//
+//  Created by Pradip Vaghasiya on 19/06/15.
+//  Copyright © 2015 SpeedUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUI.
+FOUNDATION_EXPORT double SwiftUIVersionNumber;
+
+//! Project version string for SwiftUI.
+FOUNDATION_EXPORT const unsigned char SwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUI/PublicHeader.h>
+
+
