7a3a3f3a47bd413294dc00755e8972ff30139165
pradipvaghasiya <ibm@Pradips-MacBook-Pro.local>
2015-06-29 14:29:48 +0530

    Clean Code

--- SwiftUI/Listing Automation/Data/ArrayWrapperType.swift
+++ SwiftUI/Listing Automation/Data/ArrayWrapperType.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-
-///TODO: Extending existing Protocols with already defined methods does not work. 
-///TODO: items need to be made public though this should hide this
-
 public protocol ArrayWrapperType : CollectionType,ExtensibleCollectionType,RangeReplaceableCollectionType,ArrayLiteralConvertible{
    typealias Element
    var items : [Element] {get set}
