458b89da30696f8bdc55ce82e14f85bcad87ba46
Pradip Vaghasiya <pvaghasi@in.ibm.com>
2015-10-28 12:48:50 +0000

    Bug fix collection and table view will be assigned to cell before it is configured.

--- SwiftUI/Listing Automation/Collection/SPCollectionViewDataSource.swift
+++ SwiftUI/Listing Automation/Collection/SPCollectionViewDataSource.swift
@@ -12,37 +12,37 @@ import UIKit
 ///
 ///Delegate must conform to SPListingViewProtocol
 public class SPCollectionViewDataSource : NSObject, UICollectionViewDataSource, SPCollectionListingDataSourceType{
-   
-   unowned public let controller : SPCollectionListingControllerType
-   
-   public init(_ controller : SPCollectionListingControllerType){
-      self.controller = controller
-   }
-   
-   // MARK: Number Of Sections
-   final public func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int{
-      return controller.collectionListingData(collectionView).count
-   }
-   
-   // MARK: Number Of Rows in Section
-   final public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-      return controller.collectionListingData(collectionView)[section].count
-   }
-   
-   // MARK: cellForItemAtIndexPath
-   final public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
-      
-      let viewModel = controller.collectionListingData(collectionView)[indexPath.section][indexPath.row]
-      
-      let collectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(viewModel.cellId, forIndexPath: indexPath)
-      
-      if let listingCell = collectionViewCell as? SPCollectionCellProtocol{
-         listingCell.viewModel = viewModel
-         listingCell.configureCell()
-         listingCell.collectionView = collectionView
-      }
-      
-      return collectionViewCell
-   }
-   
+    
+    unowned public let controller : SPCollectionListingControllerType
+    
+    public init(_ controller : SPCollectionListingControllerType){
+        self.controller = controller
+    }
+    
+    // MARK: Number Of Sections
+    final public func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int{
+        return controller.collectionListingData(collectionView).count
+    }
+    
+    // MARK: Number Of Rows in Section
+    final public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return controller.collectionListingData(collectionView)[section].count
+    }
+    
+    // MARK: cellForItemAtIndexPath
+    final public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+        
+        let viewModel = controller.collectionListingData(collectionView)[indexPath.section][indexPath.row]
+        
+        let collectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(viewModel.cellId, forIndexPath: indexPath)
+        
+        if let listingCell = collectionViewCell as? SPCollectionCellProtocol{
+            listingCell.viewModel = viewModel
+            listingCell.collectionView = collectionView
+            listingCell.configureCell()
+        }
+        
+        return collectionViewCell
+    }
+    
 }
--- SwiftUI/Listing Automation/Table/SPTableViewDataSource.swift
+++ SwiftUI/Listing Automation/Table/SPTableViewDataSource.swift
@@ -12,71 +12,71 @@ import UIKit
 ///
 ///Delegate must conform to SPListingViewProtocol
 public class SPTableViewDataSource : NSObject, UITableViewDataSource, SPTableListingDataSourceType {
-   unowned public let controller : SPTableListingControllerType
-   
-   public init(_ controller : SPTableListingControllerType){
-      self.controller = controller
-   }
-   
-   // MARK: Number Of Sections
-   final public func numberOfSectionsInTableView(tableView: UITableView) -> Int
-   {
-      return controller.tableListingData(tableView).count
-   }
-   
-   // MARK: Number Of Rows in Section
-   final public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-   {
-      return controller.tableListingData(tableView)[section].count
-   }
-   
-   // MARK: cellForRowAtIndexPath
-   final public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-   {
-      let viewModel = controller.tableListingData(tableView)[indexPath.section][indexPath.row]
-      
-      let tableViewCell = self.createCellUsing(
-         TableView: tableView,
-         ViewModelType: viewModel,
-         IndexPath: indexPath)
-      
-      if let listingCell = tableViewCell as? SPTableCellProtocol{
-         listingCell.viewModel = viewModel
-         listingCell.configureCell()
-         listingCell.tableView = tableView
-      }
-      
-      return tableViewCell
-   }
-   
-   ///Creates or Dequeues Cell with given Cell Id at given indexPath
-   ///
-   ///:param: TableView
-   ///:param: SPListingCellGroup
-   ///:param: IndexPath
-   ///
-   ///:returns: UITableViewCell If Cell Id is not valid it returns empty default cell.
-   private func createCellUsing(TableView tableView: UITableView,
-      ViewModelType viewModel: ViewModelType,
-      IndexPath indexPath: NSIndexPath) -> UITableViewCell{
-         
-         if (tableView.dequeueReusableCellWithIdentifier(viewModel.cellId) != nil){
-            return tableView.dequeueReusableCellWithIdentifier(viewModel.cellId, forIndexPath: indexPath)
-         }else{
-            SPLogger.logError(Message: "Class or Nib named \(viewModel.cellId) is not registered before dequeing or wrong prototype Cell.")
-         }
-         
-         return UITableViewCell()   // Returns empty default tabelview cell.
-   }
-   
-   
-   // MARK: Section Header & Footer Title
-   final public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-      return controller.tableListingData(tableView)[section].sectionHeader
-   }
-   
-   final public func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-      return controller.tableListingData(tableView)[section].sectionFooter
-   }
-   
+    unowned public let controller : SPTableListingControllerType
+    
+    public init(_ controller : SPTableListingControllerType){
+        self.controller = controller
+    }
+    
+    // MARK: Number Of Sections
+    final public func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return controller.tableListingData(tableView).count
+    }
+    
+    // MARK: Number Of Rows in Section
+    final public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return controller.tableListingData(tableView)[section].count
+    }
+    
+    // MARK: cellForRowAtIndexPath
+    final public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let viewModel = controller.tableListingData(tableView)[indexPath.section][indexPath.row]
+        
+        let tableViewCell = self.createCellUsing(
+            TableView: tableView,
+            ViewModelType: viewModel,
+            IndexPath: indexPath)
+        
+        if let listingCell = tableViewCell as? SPTableCellProtocol{
+            listingCell.viewModel = viewModel
+            listingCell.tableView = tableView
+            listingCell.configureCell()
+        }
+        
+        return tableViewCell
+    }
+    
+    ///Creates or Dequeues Cell with given Cell Id at given indexPath
+    ///
+    ///:param: TableView
+    ///:param: SPListingCellGroup
+    ///:param: IndexPath
+    ///
+    ///:returns: UITableViewCell If Cell Id is not valid it returns empty default cell.
+    private func createCellUsing(TableView tableView: UITableView,
+        ViewModelType viewModel: ViewModelType,
+        IndexPath indexPath: NSIndexPath) -> UITableViewCell{
+            
+            if (tableView.dequeueReusableCellWithIdentifier(viewModel.cellId) != nil){
+                return tableView.dequeueReusableCellWithIdentifier(viewModel.cellId, forIndexPath: indexPath)
+            }else{
+                SPLogger.logError(Message: "Class or Nib named \(viewModel.cellId) is not registered before dequeing or wrong prototype Cell.")
+            }
+            
+            return UITableViewCell()   // Returns empty default tabelview cell.
+    }
+    
+    
+    // MARK: Section Header & Footer Title
+    final public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return controller.tableListingData(tableView)[section].sectionHeader
+    }
+    
+    final public func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return controller.tableListingData(tableView)[section].sectionFooter
+    }
+    
 }
