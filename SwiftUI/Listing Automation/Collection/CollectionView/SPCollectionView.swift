d446618dc99d14ce1cb8d96c9b2af1b8535ec74a
Pradip Vaghasiya <pvaghasi@in.ibm.com>
2016-02-16 15:48:31 +0530

    Editing added into collectionview

--- SwiftUI/Listing Automation/Collection/CollectionView/SPCollectionView.swift
+++ SwiftUI/Listing Automation/Collection/CollectionView/SPCollectionView.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+private let kDefaultEditViewWidth : CGFloat = 80
+public protocol SPCollectionViewEditingDelegate : class{
+    func editView() -> UIView?
+}
+
 public class SPCollectionView: UICollectionView,SPListingCollectionViewType {
     
     public weak var controller : SPCollectionListingControllerType?
@@ -23,7 +28,34 @@ public class SPCollectionView: UICollectionView,SPListingCollectionViewType {
         }
     }
     
+    
     var listingDataSource : SPCollectionViewDataSource?
+    
+    //editing Cell
+    public weak var editingCell : UICollectionViewCell?
+    private var editingTouchStartPoint : CGPoint?
+    private weak var editView : UIView?
+    private var panGesture : UIPanGestureRecognizer?
+    weak var editingDelegate : SPCollectionViewEditingDelegate?
+    var enableEditing : Bool = false {
+        didSet{
+            if enableEditing{
+                guard let _ = panGesture else{
+                    panGesture = UIPanGestureRecognizer(target: self, action: "userPanned:")
+                    panGesture?.delegate = self
+                    addGestureRecognizer(panGesture!)
+                    return
+                }
+            }else{
+                guard let gesture = panGesture else{
+                    return
+                }
+                removeGestureRecognizer(gesture)
+                panGesture = nil
+            }
+        }
+    }
+    var editViewWidth : CGFloat = kDefaultEditViewWidth
     
     public required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
@@ -95,6 +127,158 @@ extension SPCollectionView{
         }
     }
     
+}
+
+//MARK: Edit Cell
+extension SPCollectionView : UIGestureRecognizerDelegate{
+    func userPanned(gesture : UIPanGestureRecognizer ){
+        
+        switch(gesture.state)
+        {
+        case .Began:
+            var touchPoint = gesture.locationInView(self)
+            guard let editCell = getCellFromTouch(touchPoint) else{
+                return
+            }
+            
+            guard let currentCell = editingCell else{
+                beginEditing(touchPoint, editCell: editCell, addEditView: true)
+                return
+            }
+            
+            guard currentCell != editCell else{
+                touchPoint.x += editViewWidth
+                beginEditing(touchPoint, editCell: editCell, addEditView: false)
+                return
+            }
+            
+            if(!removeEditView()){
+                beginEditing(touchPoint, editCell: editCell, addEditView: true)
+            }
+            
+        case .Changed:
+            let touchPoint = gesture.locationInView(self)
+            guard let currentEditingCell = getCellFromTouch(touchPoint),
+                let editingStartedWithCell = editingCell,
+                let startPoint = editingTouchStartPoint
+                where editingStartedWithCell == currentEditingCell else{
+                    editingGestureEnded(touchPoint)
+                    return
+            }
+            moveCell(editingStartedWithCell, offset: touchPoint.x - startPoint.x)
+        case .Ended, .Cancelled, .Failed:
+            let touchPoint = gesture.locationInView(self)
+            editingGestureEnded(touchPoint)
+        case .Possible:
+            break
+        }
+    }
+    
+    func beginEditing(touchPoint : CGPoint, editCell: UICollectionViewCell, addEditView: Bool){
+        editingTouchStartPoint = touchPoint
+        editingCell = editCell
+        if(addEditView) {
+            editView = addEditViewForCell(editCell)
+        }
+    }
+    
+    public override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let customGesture = panGesture where customGesture == gestureRecognizer else{
+            removeEditView()
+            return true
+        }
+        
+        let velocityPoint = customGesture.velocityInView(self)
+        guard velocityPoint.x < 0 else {
+            removeEditView()
+            return false
+        }
+        
+        return true
+    }
+        
+    func moveCell(cell : UICollectionViewCell, offset : CGFloat){
+        guard let editView = editView where offset > -200 && offset < 0 else{
+            return
+        }
+        
+        UIView.animateWithDuration(0, animations: {
+            cell.frame.origin.x = offset
+            editView.frame.origin.x = cell.frame.origin.x + cell.frame.width
+            editView.frame.size.width = -cell.frame.origin.x
+        })
+        
+    }
+    
+    func editingGestureEnded(point : CGPoint){
+        guard let currentCell = editingCell,
+            let editView = editView,
+            let startPoint = editingTouchStartPoint else{
+                return
+        }
+        
+        guard abs(startPoint.x - point.x) > 80 else{
+            removeEditView()
+            return
+        }
+        
+        UIView.animateWithDuration(0.3, animations: {
+            currentCell.frame.origin.x = -self.editViewWidth
+            editView.frame.origin.x = currentCell.frame.origin.x + currentCell.frame.width
+            editView.frame.size.width = -currentCell.frame.origin.x
+        })
+    }
+    
+    func removeEditView() -> Bool{
+        guard let currentCell = editingCell,
+            let editView = editView else{
+                return false
+        }
+        
+        self.editView = nil
+        self.editingCell = nil
+        self.editingTouchStartPoint = nil
+        
+        UIView.animateWithDuration(0.3, animations: {
+            currentCell.frame.origin.x = 0
+            editView.frame.origin.x = currentCell.frame.origin.x + currentCell.frame.width
+            editView.frame.size.width = -currentCell.frame.origin.x
+            }
+            , completion: {[weak self]success in
+                editView.removeFromSuperview()
+                guard let cellBeforeAnimation = self?.editingCell
+                    where cellBeforeAnimation == currentCell else{
+                        return
+                }
+            })
+        return true
+    }
+    
+    func getCellFromTouch(point : CGPoint) -> UICollectionViewCell?{
+        for cell in visibleCells(){
+            let cellFrameInCollectionView = getCellFrameInCollectionView(cell.frame)
+            if(CGRectContainsPoint(cellFrameInCollectionView, point)){
+                return cell
+            }
+        }
+        return nil
+    }
+    
+    func getCellFrameInCollectionView(cellFrame : CGRect) -> CGRect{
+        return convertRect(cellFrame, fromCoordinateSpace: self)
+    }
+
+    func addEditViewForCell(cell : UICollectionViewCell) -> UIView?{
+        guard let editView = editingDelegate?.editView() else{
+            return nil
+        }
+        var editViewFrame = getCellFrameInCollectionView(cell.frame)
+        editViewFrame.origin.x = editViewFrame.width
+        editViewFrame.size.width = 0
+        editView.frame = editViewFrame
+        addSubview(editView)
+        return editView
+    }
 }
 
 
